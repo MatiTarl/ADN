518fdf5f4e8fa8ca196f647ad53cfdf8e1f96b66
unknown <matiasvalentintari123@gmail.com>
2023-11-07 13:11:01 -0300

    configuracion

--- src/Components/LandingPage/EndPage/EndPage.jsx
+++ src/Components/LandingPage/EndPage/EndPage.jsx
@@ -1,8 +1,19 @@
-function EndPage () {
+function EndPage() {
     return (
         <main className="flex w-screen border border-black bottom-0 ">
-            <div className="p-1 m-1">
-                Informacion de contacto y demas
+            <div className="p-1 m-1 h-[40rem] flex ">
+                <div className=" border-t-emerald-100 border-1 border">
+                    Logo
+                </div>
+                <div className=" border-t-emerald-100 border-1 border">
+                    Info de contacto
+                </div>
+                <div className=" border-t-emerald-100 border-1 border">
+                    direccion
+                </div>
+                <div className=" border-t-emerald-100 border-1 border">
+                    Tarjetas trabajadas
+                </div>
             </div>
         </main>
     )
